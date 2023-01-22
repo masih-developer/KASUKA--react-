1fa90ca2b36ca11aa04cabbcd59bee6f980713b9
masih abedini <masih7816@gmail.com>
2023-01-22 12:32:01 +0330

    create components directory and placing directories inside it

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import Header from "./Header/Header";
-import GoTopBtn from "./GoTopBtn/GoTopBtn";
-import Home from "./Home/Home";
-import AboutUs from "./AboutUs/AboutUs";
-import Brands from "./Brands/Brands";
-import Feature from "./Feature/Feature";
-import Services from "./Services/Services";
-import Newsletter from "./Newsletter/Newsletter";
-import WorkSample from "./WorkSample/WorkSample";
-import Honors from "./Honors/Honors";
-import Testimonials from "./Testimonials/Testimonials";
-import OurTeam from "./OurTeam/OurTeam";
-import ContactUs from "./ContactUs/ContactUs";
-import Footer from "./Footer/Footer";
+import Header from "./components/Header/Header";
+import GoTopBtn from "./components/GoTopBtn/GoTopBtn";
+import Home from "./components/Home/Home";
+import AboutUs from "./components/AboutUs/AboutUs";
+import Brands from "./components/Brands/Brands";
+import Feature from "./components/Feature/Feature";
+import Services from "./components/Services/Services";
+import Newsletter from "./components/Newsletter/Newsletter";
+import WorkSample from "./components/WorkSample/WorkSample";
+import Honors from "./components/Honors/Honors";
+import Testimonials from "./components/Testimonials/Testimonials";
+import OurTeam from "./components/OurTeam/OurTeam";
+import ContactUs from "./components/ContactUs/ContactUs";
+import Footer from "./components/Footer/Footer";
 
 export default function App() {
     return (
